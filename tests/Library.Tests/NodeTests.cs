498d0b870906656ce2e481de6690a3b6eea16a76
Juana López <juana.lopez@correo.ucu.edu.uy>
2023-11-16 23:57:45 -0300

    tests correctos

--- tests/Library.Tests/NodeTests.cs
+++ tests/Library.Tests/NodeTests.cs
@@ -14,19 +14,4 @@ public class NodeTests
         int expected = 1;
         Assert.That(lucassNode.Children.Count, Is.EqualTo(expected));
     }
-
-    [Test]
-    public void Accept()
-    {
-        NameVisitor name = new NameVisitor();
-        Person lucas = new Person("Lucas", 19);
-        Person maria = new Person("Maria Jose", 20);
-        Node<Person> lucassNode = new Node<Person>(lucas);
-        Node<Person> mariasNode = new Node<Person>(maria);
-        lucassNode.AddChildren(mariasNode);
-        name.Visit(lucassNode);
-
-        string expected = "Maria Jose";
-        Assert.That(name.longestName, Is.EqualTo(expected));
-    }
 }
